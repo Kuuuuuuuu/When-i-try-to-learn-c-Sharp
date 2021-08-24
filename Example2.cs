e173406cc39ee29bbc7ea0a68c36a439da065876
MelidaDev! <69802296+MelidaZ@users.noreply.github.com>
2021-08-24 10:06:11 +0700

    Make For loops (Complete)

---
+++ Example2.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Functions {
+    public static void Main() {
+        string text = "This is for loops";
+        int n = 10;
+        for( int i = 0; i < n; i++) {
+            Console.WriteLine(text);
+        }
+    }
+}
