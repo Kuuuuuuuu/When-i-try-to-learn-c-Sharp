d84924bbee87710581a37b3a40de30f71c207967
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2021-11-05 11:18:25 +0700

    Create Enum.cs

---
+++ Enum.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum {
+ class Program  {
+  enum Color{Red,Green,blue,yellow,pink,black};
+
+  static void Main(string[] args) {
+   Console.Write(Days.Sun);
+   Console.ReadKey();
+  }
+ }
+}
